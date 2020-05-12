f3e34284cf93b658c57dbde7c091c718e3a6ba7d
jniyfish <39803423+jniyfish@users.noreply.github.com>
2020-05-12 23:54:07 +0800

    Add files via upload

---
+++ test_prep.py
@@ -0,0 +1,19 @@
+import csv
+import os,shutil
+
+
+path ='./C1-P1_Train Dev_fixed/C1-P1_Dev/'
+moveto = './test_data/'
+
+with open('./C1-P1_Train Dev_fixed/dev.csv',newline='') as csvfile:
+
+	rows = csv.reader(csvfile)
+	for row in rows :
+		if row[0] == 'image_id':
+			continue
+		str1 = row[0]
+		str2 = row[1]
+		src = path+str1
+		dst = moveto+str2
+		shutil.move(src,dst)
+
